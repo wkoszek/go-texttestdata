b9a819fbed56153f210bd02780452449e482a934
Wojciech Adam Koszek <wojciech@koszek.com>
2019-05-26 21:33:02 -0700

    Regen

--- regen.sh
+++ regen.sh
@@ -4,5 +4,5 @@
 
 (cd text/ && /bin/ls -1 *.txt | while read T; do
   O=${T%%.txt}
-  ../tools/data2golang --pkgname gotestdata $T $O > ../${O}.go
+  ../tools/data2golang --pkgname gotexttestdata $T $O > ../${O}.go
 done)
