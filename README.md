ccf0021042a0eaa5f37512bf38b9eff316aedb6d
Wojciech Adam Koszek <wojciech@koszek.com>
2019-05-26 21:34:16 -0700

    Make naming consistent.

--- README.md
+++ README.md
@@ -11,25 +11,7 @@ Install the package:
 
 ## Example
 
-	package main
-
-	import "fmt"
-	import "github.com/wkoszek/go-texttestdata"
-
-	func main() {
-
-		// To print out a big body of text from each of the public
-		// domain books, you can use:
-
-		fmt.Println(string(gotestdata.MobyDick));
-		fmt.Println(string(gotestdata.ATaleOfTwoCities));
-		fmt.Println(string(gotestdata.Dracula));
-		fmt.Println(string(gotestdata.GrimsFairyTales));
-		fmt.Println(string(gotestdata.Metamorphosis));
-		fmt.Println(string(gotestdata.PictureOfDorianGrey));
-	}
-
-There is an example which you can build:
+There is an example in [examples](examples) directory which you can build:
 
 	cd ./examples/
 	go build example.go
--- examples/example.go
+++ examples/example.go
@@ -8,10 +8,10 @@ func main() {
 	// To print out a big body of text from each of the public
 	// domain books, you can use:
 
-	fmt.Println(string(gotestdata.MobyDick));
-	fmt.Println(string(gotestdata.ATaleOfTwoCities));
-	fmt.Println(string(gotestdata.Dracula));
-	fmt.Println(string(gotestdata.GrimsFairyTales));
-	fmt.Println(string(gotestdata.Metamorphosis));
-	fmt.Println(string(gotestdata.PictureOfDorianGrey));
+	fmt.Println(string(gotexttestdata.MobyDick));
+	fmt.Println(string(gotexttestdata.ATaleOfTwoCities));
+	fmt.Println(string(gotexttestdata.Dracula));
+	fmt.Println(string(gotexttestdata.GrimsFairyTales));
+	fmt.Println(string(gotexttestdata.Metamorphosis));
+	fmt.Println(string(gotexttestdata.PictureOfDorianGrey));
 }
